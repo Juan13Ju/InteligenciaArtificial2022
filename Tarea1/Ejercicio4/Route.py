0452b17e3ff94398587bf0c7dcea85e463d4e6e4
SGM2099 <sebasguti1511@comunidad.unam.mx>
2022-10-17 23:09:35 -0500

    pdf con integrantes y subdivision para ej 4.1 y 4.2

--- Tarea1/Ejercicio4/Route.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-class Route(object):
-        def __init__(self, name, value, realCost, iteration, node):
-                self.name = name
-                self.value = value
-                self.realCost = realCost
-                self.iteration = iteration
-                self.node = node
